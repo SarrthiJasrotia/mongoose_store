3be883ae69c4e92b9d8746009ec16854f82e599b
Sarrthi Jasrotia <sarrthijasrotia05@gmail.com>
2022-09-07 20:57:13 -0500

    added info on the index page

---
+++ controller/controller.js
@@ -0,0 +1,59 @@
+const { application } = require('express');
+const express = require('express');
+const Product = require('../models/product')
+const productRouter = express.Router()
+
+////ROUTES////
+
+// INDEX
+productRouter.get("/",(req,res)=>{
+    Product.find({},(error,allProducts)=>{
+        res.render("index.ejs",{
+            productsIndex: allProducts,
+        });
+    });
+});
+// NEW
+productRouter.get('/new',(req,res)=>{
+    res.render('new.ejs')
+})
+// DELETE
+productRouter.delete("/:id",(req,res)=>{
+    Product.findByIdAndDelete(req.params.id, (error, data)=>{
+        res.redirect('/products')
+    });
+});
+// UPDATE
+productRouter.put('/id:',(req,res)=>{
+    Product.findByIdAndUpdate(req.params.id,req.body, {
+        new:true,
+    },
+    (error,updatedProduct)=>{
+        res.redirect(`/products/${req.params.id}`)
+    });
+});
+// CREATE
+productRouter.post('/', (req, res) => {
+    Product.create(req.body, (error, listedProduct) => {
+        res.redirect('/products');
+    });
+})
+// EDIT
+productRouter.get('/:id/edit',(req,res)=>{
+    Product.findById(req.params.id, (error, listedProduct)=>{
+        res.render('edit.ejs', {
+            productsEdit: listedProduct,
+        });
+    });
+});
+// SHOW
+productRouter.get('/:id',(req,res)=>{
+    Product.findById(req.params.id,(error,allProducts)=>{
+        res.render('show.ejs',{
+            productsShow: allProducts,
+        });
+    });
+});
+
+
+module.exports = productRouter
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const mongoose = require("mongoose")
-const Product = require('./models/product')
 const methodOverride = require("method-override")
 
 //Database config
@@ -24,44 +23,8 @@ app.use(methodOverride("_method"))
 // Body parser middleware: give us access to req.body
 app.use(express.urlencoded({ extended: true }));
 
-////ROUTES////
-
-// INDEX
-app.get("/products",(req,res)=>{
-    Product.find({},(error,allProducts)=>{
-        res.render("index.ejs",{
-            productsIndex: allProducts,
-        });
-    });
-});
-// NEW
-app.get('/products/new',(req,res)=>{
-    res.render('new.ejs')
-})
-// DELETE
-app.delete("/products/:id",(req,res)=>{
-    Product.findByIdAndDelete(req.params.id, (error, data)=>{
-        res.redirect('/products')
-    });
-});
-// UPDATE
-
-// CREATE
-app.post('/products', (req, res) => {
-    Product.create(req.body, (error, listedProduct) => {
-        res.redirect('/products');
-    });
-})
-// EDIT
-
-// SHOW
-app.get('/products/:id',(req,res)=>{
-    Product.findById(req.params.id,(error,allProducts)=>{
-        res.render('show.ejs',{
-            productsShow: allProducts,
-        });
-    });
-});
+const productController = require("./controller/controller")
+app.use("/products",productController)
 
 // Listener
 const PORT = process.env.PORT;
